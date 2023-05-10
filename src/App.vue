9dd0f75e7c7f8346fb9416171d7fd253479d8bca
Kartik Riziya <kartikriziya30721@gmail.com>
2023-05-10 20:01:40 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,24 @@
 <template>
-<Header />
+  <Header />
 
-<Main />
+  <Main />
 
-<Footer />
+  <Footer />
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import Main from './components/Main.vue'
-import Footer from './components/Footer.vue'
+import Header from "./components/Header.vue"
+import Main from "./components/Main.vue"
+import Footer from "./components/Footer.vue"
 
 export default {
-    name: 'App',
-    components: {
-        Header,
-        Main,
-        Footer
-    }
+  name: "App",
+  components: {
+    Header,
+    Main,
+    Footer,
+  },
 }
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
